8d542fffa0fdb616d87c61ef4687e2d72de2c8b1
Anubhuti Sharma <138015308+anubhutisharma07@users.noreply.github.com>
2023-11-23 17:58:24 +0530

    Add files via upload

---
+++ number_method.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+abs(10.5)
+
+
+# In[8]:
+
+
+abs(-23.6)
+
+
+# In[12]:
+
+
+import math
+math.ceil(21.1)
+
+
+# In[13]:
+
+
+math.ceil(21.5)
+
+
+# In[14]:
+
+
+math.floor(20.3)
+
+
+# In[15]:
+
+
+math.floor(-23.4)
+
+
+# In[16]:
+
+
+math.exp(2)
+
+
+# In[17]:
+
+
+math.fabs(-21.35)
+
+
+# In[20]:
+
+
+math.fabs(9.1) ## can not use in complex numbers
+
+
+# In[21]:
+
+
+math.log(2)
+
+
+# In[22]:
+
+
+max(12,34,56)
+
+
+# In[23]:
+
+
+print(max(-23,-45,-56))
+
+
+# In[24]:
+
+
+min(1,3,5,7)
+
+
+# In[25]:
+
+
+print(min(-2,-23,-45))
+
+
+# In[26]:
+
+
+import math
+math.pow(2,3)
+
+
+# In[27]:
+
+
+math.pow(20,3)
+
+
+# In[28]:
+
+
+math.sqrt(25)
+
+
+# In[29]:
+
+
+math.sqrt(2)
+
+
+# In[30]:
+
+
+math.sin(0)
+
+
+# In[31]:
+
+
+math.sin(90)
+
+
+# In[32]:
+
+
+math.hypot(2,3)
+
+
+# In[ ]:
+
+
+
+
